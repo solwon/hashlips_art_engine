b01d27782b3c2af15105b0c914f6c8bb03e69897
Sol Won <haesol3@naver.com>
2022-09-19 21:15:53 +0900

    another finalized

--- src/config.js
+++ src/config.js
@@ -27,8 +27,8 @@ const solanaMetadata = {
 const layerConfigurations = [
   {
     isSelection: true,
-    growEditionSizeTo: pickedDna.length,
-    // growEditionSizeTo: 0,
+    // growEditionSizeTo: pickedDna.length,
+    growEditionSizeTo: 0,
     layersOrder: [
       {
         name: "Divinity",
@@ -62,37 +62,37 @@ const layerConfigurations = [
       { name: "Mouth" },
     ],
   },
-  // {
-  //   isSelection: false,
-  //   growEditionSizeTo: 1600,
-  //   layersOrder: [
-  //     {
-  //       name: "Divinity",
-  //       options: {
-  //         bypassDNA: true,
-  //       },
-  //     },
-  //     { name: "Background" },
-  //     { name: "Sign" },
-  //     {
-  //       name: "Fur_YesEars",
-  //       options: {
-  //         displayName: "Fur",
-  //       }
-  //     },
-  //     { name: "Tabby" },
-  //     {
-  //       name: "Headwear_YesEars",
-  //       options: {
-  //         displayName: "Headwear"
-  //       }
-  //     },
-  //     { name: "Clothing" },
-  //     { name: "Eyes" },
-  //     { name: "Eyewear" },
-  //     { name: "Mouth" },
-  //   ],
-  // },
+  {
+    isSelection: false,
+    growEditionSizeTo: 4000,
+    layersOrder: [
+      {
+        name: "Divinity",
+        options: {
+          bypassDNA: true,
+        },
+      },
+      { name: "Background" },
+      { name: "Sign" },
+      {
+        name: "Fur_YesEars",
+        options: {
+          displayName: "Fur",
+        }
+      },
+      { name: "Tabby" },
+      {
+        name: "Headwear_YesEars",
+        options: {
+          displayName: "Headwear"
+        }
+      },
+      { name: "Clothing" },
+      { name: "Eyes" },
+      { name: "Eyewear" },
+      { name: "Mouth" },
+    ],
+  },
   // {
   //   isSelection: false,
   //   growEditionSizeTo: 4000,
